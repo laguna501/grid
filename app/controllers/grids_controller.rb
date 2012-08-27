ead66a7668901fad96181968105a4f24604e0c86
Artiwarah Damrongchai <artiwarahd@swiftlet.co.th>
2012-08-27 16:42:11 +0700

    #34206095
    Change access_token for test

--- app/controllers/grids_controller.rb
+++ app/controllers/grids_controller.rb
@@ -9,9 +9,10 @@ class GridsController < ActionController::Base
 
     app = FbGraph::Application.new(135259466618586, :secret => '5c7369efc1f535f76e7640779cfd97e4')
     users = [
-      FbGraph::User.fetch('artiwarah', :access_token => 'AAACEdEose0cBAFFGRRsK4zrZAqV11wocYe6uLiSGK3otS2SA71MksGh3pHcI1HN7KwUSSAXBGWeVkicvRxHZCU81OVgor89rrTmXza2lIt1I1RPSNG'),
-      FbGraph::User.fetch('chanisa.suwannarang', :access_token => 'AAACEdEose0cBAFJJtVDMaCvqcZCGUQCB4TXibPDjtr3dbJjmVkf9kly5PEqPlmxpQmB8zPDwXhyEGbts5gFSWeLgRzZATDFXeyEWIThWlVfCDFVZC70'),
-      FbGraph::User.fetch('eadheat', :access_token => 'AAACEdEose0cBAJL8OVZALgVO8P1Ptih7HGnMJYUZA3YQKpFZBWVv6uefn2rzuaKTZBZCn1b8sYURU6PFy13v9W48PKeArfVXwaB4CpuJ8OUizdfHKv3I9')
+      FbGraph::User.fetch('artiwarah', :access_token => 'AAACEdEose0cBACWJwsjtW7UdSSw1R804w5dkxwcEa1XcSM0y2ZB29bPkgyjplNsZByVuxSM7wYieC41YyvUYoyYHSRAJRyd15AfrpAPyRtxFmrG1e2'),
+      FbGraph::User.fetch('chanisa.suwannarang', :access_token => 'AAACEdEose0cBACZB5Hqkxar1qjqHoVik3qGDsSkVLkaSw5UZCVil87tZBv59crp0NMZBRxcdP1IIFJqnu2MfPrTMHhgaIz0lnzIliU1tcVryrXROnNWB'),
+      FbGraph::User.fetch('eadheat', :access_token => 'AAACEdEose0cBAPw24ZA1ROfnTnESrZBg8qY2gVFJFZCntnCZArXKFlFZBBqjL0RhftDwbrZClvDqNHRClGLKeBiemnH6dGmiOaNKavfIhUqxP9uLDG44cv'),
+      FbGraph::User.fetch('pinmanee.chaisawat', :access_token => 'AAACEdEose0cBAIjdPt6iGpYtrDPGyHE6NV3tpw5wc86ZCf93ZBAThZAQ8xBRUTxWbHdMaWs5ZBvTcoZAn2QQEqdqpZBbveGrUcVa5Bzuh3SV3jeJ7itmOS')
     ]
 
     @user_profile_pictures = Hash.new
@@ -25,9 +26,10 @@ class GridsController < ActionController::Base
 
   def show_by_user
     access_tokens = {
-      'artiwarah' => 'AAACEdEose0cBAFFGRRsK4zrZAqV11wocYe6uLiSGK3otS2SA71MksGh3pHcI1HN7KwUSSAXBGWeVkicvRxHZCU81OVgor89rrTmXza2lIt1I1RPSNG',
-      'chanisa.suwannarang' => 'AAACEdEose0cBAFJJtVDMaCvqcZCGUQCB4TXibPDjtr3dbJjmVkf9kly5PEqPlmxpQmB8zPDwXhyEGbts5gFSWeLgRzZATDFXeyEWIThWlVfCDFVZC70',
-      'eadheat' => 'AAACEdEose0cBAJL8OVZALgVO8P1Ptih7HGnMJYUZA3YQKpFZBWVv6uefn2rzuaKTZBZCn1b8sYURU6PFy13v9W48PKeArfVXwaB4CpuJ8OUizdfHKv3I9'
+      'artiwarah' => 'AAACEdEose0cBACWJwsjtW7UdSSw1R804w5dkxwcEa1XcSM0y2ZB29bPkgyjplNsZByVuxSM7wYieC41YyvUYoyYHSRAJRyd15AfrpAPyRtxFmrG1e2',
+      'chanisa.suwannarang' => 'AAACEdEose0cBACZB5Hqkxar1qjqHoVik3qGDsSkVLkaSw5UZCVil87tZBv59crp0NMZBRxcdP1IIFJqnu2MfPrTMHhgaIz0lnzIliU1tcVryrXROnNWB',
+      'eadheat' => 'AAACEdEose0cBAPw24ZA1ROfnTnESrZBg8qY2gVFJFZCntnCZArXKFlFZBBqjL0RhftDwbrZClvDqNHRClGLKeBiemnH6dGmiOaNKavfIhUqxP9uLDG44cv',
+      'pinmanee.chaisawat' => 'AAACEdEose0cBAIjdPt6iGpYtrDPGyHE6NV3tpw5wc86ZCf93ZBAThZAQ8xBRUTxWbHdMaWs5ZBvTcoZAn2QQEqdqpZBbveGrUcVa5Bzuh3SV3jeJ7itmOS'
     }
 
     unless params[:user_identifier].blank?
