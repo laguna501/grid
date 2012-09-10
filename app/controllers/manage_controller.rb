1e356c84e09829f3ac0f21901aa37a58b1960530
Somporn Bunnamol <sompornb@swiftlet.co.th>
2012-09-10 15:25:35 +0700

    #35424559
    Instagram integration

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -82,6 +82,23 @@ class ManageController < ActionController::Base
 
   def pull_photos_instagram
     accounts = Account.includes(:photos).where("access_token IS NOT NULL").where(social_type: "instagram")
+    accounts.each do |account|
+      client = Instagram.client(:access_token => account.access_token)
+      all_photo = account.photos.map(&:identifier)
+      medias = client.user_recent_media
+      medias.each do |media|
+        next if all_photo.include?(media.id)
+        next if media.caption.blank?
+        next unless media.caption.text =~ /#grid/
+        photo = Photo.new
+        photo.identifier = media.id
+        photo.account_id = account.id
+        photo.thumbnail = photo.save_file(account.username, media.images.low_resolution.url, account.social_type)
+        photo.full = photo.save_file(account.username, media.images.standard_resolution.url, account.social_type)
+        photo.save
+      end
+    end
+    redirect_to grids_url
   end
 
   private
