54f0a23ae4b3c2bac025f3610635552911f3595b
Artiwarah Damrongchai <artiwarahd@swiftlet.co.th>
2012-09-17 16:37:37 +0700

    #34204597
    change filter display from username to nickname

--- spec/factories/account_factory.rb
+++ spec/factories/account_factory.rb
@@ -3,6 +3,6 @@ FactoryGirl.define do
   	user
     username {"username"}
     access_token { "AAAB7BIx11toBAGZBkDTZA4ZChV7DdUAQE8pSRTsBGdbr49Gg64opGwvVYyr62jcSEwedsnNB6RdFlZBVgCgZAAmc7zU92hD9QChFBB669WQZDZD"}
-    social_type       { "facebook"}
+    type       { "FacebookAccount"}
   end
 end
