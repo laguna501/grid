6735c74dfd91f8c30b1cb23f65a40e86339b6577
Artiwarah Damrongchai <artiwarahd@swiftlet.co.th>
2012-09-21 11:06:07 +0700

    #34059375
    remove private from extend_access_token method

--- app/models/facebook_account.rb
+++ app/models/facebook_account.rb
@@ -4,5 +4,4 @@ class FacebookAccount < Account
   def extend_access_token
     Notifier.extend_facebook_access_token(self).deliver
   end
-  private :extend_access_token
 end
--- app/models/instagram_account.rb
+++ app/models/instagram_account.rb
@@ -1,8 +1,7 @@
 class InstagramAccount < Account
   after_create :extend_access_token
-  
+
   def extend_access_token
     Notifier.extend_instagram_access_token(self).deliver
   end
-  private :extend_access_token
 end
