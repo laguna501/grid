defda1a4ad89e5907cd37b2abfa1fc51e9a0972e
Arthit Hongchintakul <arthith@swiftlet.co.th>
2012-10-03 22:28:02 +0700

    33258289
    add production environment information

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,13 +69,13 @@ GRID::Application.configure do
 
   config.upload_path = Rails.root.join(*%w(.. shared uploads))
 
-  config.notifier_default_host = "grid.swiftlet.co.th"
+  config.notifier_default_host = "grid.canonlife.com"
 
   config.fb_app_id = "135259466618586"
   config.fb_app_secret = "5c7369efc1f535f76e7640779cfd97e4"
-  config.fb_callback_url = "https://grid.swiftlet.co.th/facebook/callback"
+  config.fb_callback_url = "https://grid.canonlife.com/facebook/callback"
 
   config.ig_client_id = "cf32424d71d4445995bf58ec0e2a3bd2"
   config.ig_client_secret = "5e0be7f1d1b74102acb324183cdf6c3f"
-  config.ig_callback_url = "https://grid.swiftlet.co.th/instagram/callback"
+  config.ig_callback_url = "https://grid.canonlife.com/instagram/callback"
 end
