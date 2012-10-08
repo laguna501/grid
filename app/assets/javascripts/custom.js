46e8703375e52091a37d1e8676a180c071e7785a
Arthit Hongchintakul <arthith@swiftlet.co.th>
2012-10-08 17:56:15 +0700

    fix custom.js

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,9 +1,13 @@
-$(function() {
-  $("div.isotope-item").mouseover(function(){
-    $(this).children(".infos").removeClass().addClass("infos show");
-    $(this).children("a").children("img").removeClass().addClass("img-fade");
-  }).mouseout(function(){
-    $(this).children(".infos").removeClass().addClass("infos");
-    $(this).children("a").children("img").removeClass();
-  });
-});
+Custom = {
+  init: function() {
+    $("div.isotope-item").mouseover(function(){
+      $(this).children(".infos").removeClass().addClass("infos show");
+      $(this).children("a").children("img").removeClass().addClass("img-fade");
+    }).mouseout(function(){
+      $(this).children(".infos").removeClass().addClass("infos");
+      $(this).children("a").children("img").removeClass();
+    });
+  }
+}
+
+$(Custom.init);
