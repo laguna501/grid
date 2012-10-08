e4ff243c9701352cb7084f3a805f5e19453fa7a0
Arthit Hongchintakul <arthith@swiftlet.co.th>
2012-10-08 17:48:35 +0700

    add custom javascript

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,9 @@
+$(function() {
+  $("div.isotope-item").mouseover(function(){
+    $(this).children(".infos").removeClass().addClass("infos show");
+    $(this).children("a").children("img").removeClass().addClass("img-fade");
+  }).mouseout(function(){
+    $(this).children(".infos").removeClass().addClass("infos");
+    $(this).children("a").children("img").removeClass();
+  });
+});
