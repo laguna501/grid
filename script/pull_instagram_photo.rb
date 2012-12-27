780776ab8b2bf65915f6c38f0f0bcd19d23af55b
laguna501 <laguna501@gmail.com>
2012-12-27 23:35:45 +0700

    Revert "Revert "Correct folder of Instagram Folder""
    This reverts commit 58fdb478da2d41390524f3dae8f1ca70b7cc45d2.

--- script/pull_instagram_photo.rb
+++ script/pull_instagram_photo.rb
@@ -11,8 +11,8 @@ accounts.each do |account|
       next unless media.caption.text =~ /#grid/i
       account.photos.create(
         identifier: media.id,
-        full: Photo.save_file(account.username, media.images.low_resolution.url, account.social_type, "full"),
-        thumbnail: Photo.save_file(account.username, media.images.standard_resolution.url, account.social_type, "thumbnail")
+        full: Photo.save_file(account.username, media.images.standard_resolution.url, account.social_type, "full"),
+        thumbnail: Photo.save_file(account.username, media.images.low_resolution.url, account.social_type, "thumbnail")
       )
     end
     count = medias.count
