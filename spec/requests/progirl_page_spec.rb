8305bde5ce7b496e977ced0cecd938f20e0b764e
Chanisa Suwannarang <chanisas@swiftlet-008.local>
2013-01-29 13:44:19 +0700

    Edit code

--- spec/requests/progirl_page_spec.rb
+++ spec/requests/progirl_page_spec.rb
@@ -3,14 +3,6 @@ require 'request_spec_helper'
 
 describe "Test Pro Grid and Girl Grid page", js: true do
   describe "Pro page" do
-    it "I want to see Pro page when I clicked button" do
-      visit ("/")
-      current_path.should == "/"
-      click_link ('Pro Grid')
-      current_path == "/grids/show_users?type=pro"
-      # page.should have_css("#show_users", :text => "Pro Grid")
-    end
-
     it "I want to see photos from Facebook in Pro page" do
       user = FactoryGirl.create(:user, email: "artiwarah@example.com",user_type: "pro", nickname: "JOE")
       account = FactoryGirl.create(:account,username: "artiwarah", type: "FacebookAccount", user: user)
@@ -21,10 +13,7 @@ describe "Test Pro Grid and Girl Grid page", js: true do
         account: account
         )
 
-      visit ("/")
-      current_path.should == "/"
-      page.should have_css("#grid" , :text => "Pro Grid")
-      click_link ('Pro Grid')
+      visit("/grids/show_users?type=pro")
       page.should have_css("img[@alt='182869_3569852204790_1354565865_n']")
     end
 
@@ -38,22 +27,12 @@ describe "Test Pro Grid and Girl Grid page", js: true do
         account: account
         )
 
-      visit ("/")
-      current_path.should == "/"
-      page.should have_css("#grid" , :text => "Pro Grid")
-      click_link ('Pro Grid')
+      visit("/grids/show_users?type=pro")
       page.should have_css("img[@alt='04112eeafbca11e1968922000a1cf73c_7']")
     end
   end
 
   describe "Girl page" do
-  	it "As an user I want to see Girl page when I clicked button" do
-  	  visit ("/")
-  	  current_path.should == "/"
-      click_link ('Girl Grid')
-      current_path == "/grids/show_users?type=girl"
-      page.should have_css("#show_users", :text => "Girl Grid")
-  	end
     it "I want to see photos from Facebook in Girl page" do
       user = FactoryGirl.create(:user, email: "artiwarah@example.com",user_type: "girl", nickname: "JOE")
       account = FactoryGirl.create(:account,username: "artiwarah", type: "FacebookAccount", user: user)
@@ -64,10 +43,7 @@ describe "Test Pro Grid and Girl Grid page", js: true do
         account: account
         )
 
-      visit ("/")
-      current_path.should == "/"
-      page.should have_css("#grid" , :text => "Girl Grid")
-      click_link ('Girl Grid')
+      visit("/grids/show_users?type=girl")
       page.should have_css("img[@alt='182869_3569852204790_1354565865_n']")
     end
     it "I want to see photos from Instagram in Girl page" do
@@ -80,10 +56,7 @@ describe "Test Pro Grid and Girl Grid page", js: true do
         account: account
         )
 
-      visit ("/")
-      current_path.should == "/"
-      page.should have_css("#grid" , :text => "Girl Grid")
-      click_link ('Girl Grid')
+      visit("/grids/show_users?type=girl")
       page.should have_css("img[@alt='9f599bd6fbc911e1a44612313804e8c1_7']")
     end
   end
@@ -99,10 +72,7 @@ describe "Test Pro Grid and Girl Grid page", js: true do
         account: account
         )
 
-      visit ("/")
-      current_path.should == "/"
-      # page.should have_css("#grid" , :text => "Girl Grid")
-      click_link ('Girl Grid')
+      visit("/grids/show_users?type=girl")
       page.find("img[@alt='182869_3569852204790_1354565865_n']").click
     end
   end
--- spec/requests/show_page_spec.rb
+++ spec/requests/show_page_spec.rb
@@ -6,28 +6,26 @@ describe "Test show page", js: true  do
     it "As an user I see photo from Facebook in view page" do
       user = FactoryGirl.create(:user, email: "artiwarah@example.com",user_type: "pro", nickname: "JOE")
       account = FactoryGirl.create(:account,username: "artiwarah", type: "FacebookAccount", user: user)
-      photo = FactoryGirl.create(:photo, 
+      photo = FactoryGirl.create(:photo,
         identifier: '9',
         thumbnail: "/assets/uploads/facebook/thumbnail/artiwarah/182869_3569852204790_1354565865_n.jpg",
         full: "/assets/uploads/facebook/full/artiwarah/182869_3569852204790_1354565865_n.jpg", account: account)
 
       visit ("/grids/show_photo?identifier=9&owner=JOE")
-      page.should have_css("#nickname", :text => "JOE")
       page.should have_css("img[@alt='182869_3569852204790_1354565865_n']")
     end
 
     it "As an user I see photo from Instagram" do
      user = FactoryGirl.create(:user, email: "Chanisa@example.com",user_type: "girl", nickname: "FERN")
       account = FactoryGirl.create(:account,username: "fernzzzaa", type: "InstagramAccount", user: user)
-      photo = FactoryGirl.create(:photo, 
+      photo = FactoryGirl.create(:photo,
         identifier: '10',
         thumbnail: "/assets/uploads/instagram/thumbnail/fernzzzaa/9f599bd6fbc911e1a44612313804e8c1_7.jpg",
-        full: "/assets/uploads/instagram/full/fernzzzaa/9f599bd6fbc911e1a44612313804e8c1_6.jpg", 
+        full: "/assets/uploads/instagram/full/fernzzzaa/9f599bd6fbc911e1a44612313804e8c1_6.jpg",
         account: account
         )
 
       visit ("/grids/show_photo?identifier=10&owner=FERN")
-      page.should have_css("#nickname", :text => "FERN")
       page.should have_css("img[@alt='9f599bd6fbc911e1a44612313804e8c1_6']")
     end
   end
